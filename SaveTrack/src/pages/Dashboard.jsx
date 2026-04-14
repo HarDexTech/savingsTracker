d11efe464bdb1ec6c0314aca360c479d82e576f5
ADESINA AYOMIDE <adesinayomide2@gmail.com>
2026-04-14 14:44:51 +0100

    Implement Dashboard component and update main rendering logic

---
+++ SaveTrack/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+function DashBoard() {
+  return (
+    <section className="bg-black h-[100dvh] w-full">
+      <aside className="container"></aside>
+    </section>
+  );
+}
+export default DashBoard;
