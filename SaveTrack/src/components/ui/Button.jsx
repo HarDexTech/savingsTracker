ffc2be2ee0ff496ff06f28068eb7bde73caaa70a
ADESINA AYOMIDE <adesinayomide2@gmail.com>
2026-04-14 17:06:32 +0100

    Refactor Dashboard to use Button component for improved code reusability and consistency

---
+++ SaveTrack/src/components/ui/Button.jsx
@@ -0,0 +1,13 @@
+function Button(children) {
+  return (
+    (children.active === "true") ?
+    <button className="text-(--text-main) bg-linear-to-r from-[#ffb59d] to-[#f06536] rounded-lg shadow-[0_0_20px_rgba(240,101,54,0.3)] flex items-center space-x-3 p-3 font-['Arial'] font-medium text-sm tracking-wide transition-transform duration-200 scale-95 active:scale-90 cursor-pointer w-full">
+      {children.destination}
+    </button>
+    :
+    <button className="text-(--text-main) rounded-lg flex items-center space-x-3 p-3 font-['Arial'] font-medium text-sm tracking-wide transition-transform duration-200 scale-95 active:scale-90 cursor-pointer w-full hover:shadow-[0_0_25px_rgba(240,101,54,0.5)]">
+      {children.destination}
+    </button>
+  );
+}
+export default Button
