39b416d76d6acd02b44543936d3c0f0373af11d2
ADESINA AYOMIDE <adesinayomide2@gmail.com>
2026-04-14 16:01:39 +0100

    Refactor Dashboard layout and enhance styling with Tailwind CSS

--- SaveTrack/src/pages/Dashboard.jsx
+++ SaveTrack/src/pages/Dashboard.jsx
@@ -1,9 +1,18 @@
 function DashBoard() {
   return (
-    <section className="bg-black h-[100dvh] w-full">
-      <aside className="container">hi</aside>
-      <aside className="container"></aside>
-      <main>Hello world</main>
+    <section className="bg-black h-dvh w-full">
+      <aside className="p-6 sidebar h-dvh w-75 bg-(--bg-surface-elevated)">
+        <h1 class="text-2xl font-black text-[#ffb59d] uppercase tracking-tighter">
+          SaveTrack
+        </h1>
+
+        <div className="btnContainer flex flex-col items-start py-6">
+          <button className="text-(--text-main) bg-linear-to-r from-[#ffb59d] to-[#f06536] rounded-lg shadow-[0_0_20px_rgba(240,101,54,0.3)] flex items-center space-x-3 p-3 font-['Inter'] font-medium text-sm tracking-wide transition-transform duration-200 scale-95 active:scale-90 cursor-pointer">Dashboard</button>
+          <button className="text-(--text-main)">Goals</button>
+          <button className="text-(--text-main)">History</button>
+        </div>
+      </aside>
+      <main></main>
     </section>
   );
 }
