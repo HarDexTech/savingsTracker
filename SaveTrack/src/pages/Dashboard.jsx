bb13452ca8bc0f5b61f532024a3fd1e8950c0774
ADESINA AYOMIDE <adesinayomide2@gmail.com>
2026-04-14 17:33:54 +0100

    Refactor Button component and update Dashboard layout for improved structure and styling

--- SaveTrack/src/pages/Dashboard.jsx
+++ SaveTrack/src/pages/Dashboard.jsx
@@ -1,18 +1,24 @@
+import Button from '../components/ui/Button';
+
 function DashBoard() {
   return (
     <section className="bg-(--bg-app) h-dvh w-full flex flex-row items-start">
-      <aside className="p-6 sidebar h-dvh w-75 bg-(--bg-surface-elevated)">
-        <h1 className="text-2xl font-black text-[#ffb59d] uppercase tracking-tighter">
-          SaveTrack
-        </h1>
+      <aside className="p-6 sidebar h-dvh w-[20%] bg-(--bg-surface-elevated) flex flex-col justify-between items-start">
+        <div className="top">
+          <h1 className="text-2xl font-black text-[#ffb59d] uppercase tracking-tighter">
+            SaveTrack
+          </h1>
 
-        <div className="btnContainer flex flex-col items-start py-6 gap-3">
-          <Button destination="Dashboard" active="false"/>
-          <Button destination="Goals" active="true"/>
-          <Button destination="History" active="false"/>
+          <div className="btnContainer flex flex-col items-start py-6 gap-3">
+            <Button destination="Dashboard" active="false" />
+            <Button destination="Goals" active="true" />
+            <Button destination="History" active="false" />
+          </div>
         </div>
+
+        <Button destination="+ New Goal" active="true" />
       </aside>
-      <main></main>
+      <main className="p-6 h-dvh w-[80%] bg-(--bg-app)"></main>
 
       <header className="flex bg-(--bg-app) p-4 justify-evenly items-center gap-5">
         <h2 className="text-(--color-primary)">Dashboard Overview</h2>
@@ -24,8 +30,8 @@ function DashBoard() {
             placeholder="Search Portfolio"
             className="w-98 h-10 bg-(--bg-surface-elevated) rounded-full text-(--text-muted) px-4 outline-none"
           />
-          <i class="fas fa-bell text-(--text-muted) text-2xl"></i>
-          <i class="fas fa-cog text-(--text-muted) text-2xl"></i>
+          <i className="fas fa-bell text-(--text-muted) text-2xl"></i>
+          <i className="fas fa-cog text-(--text-muted) text-2xl"></i>
         </span>
       </header>
     </section>
