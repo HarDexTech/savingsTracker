f3befc36179078ac533505bd16110ab670b5a788
KoryTECH <korylinemedia@gmail.com>
2026-04-14 15:23:05 +0100

    u

--- SaveTrack/src/pages/Dashboard.jsx
+++ SaveTrack/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ function DashBoard() {
   return (
     <section className="bg-black h-[100dvh] w-full">
       <aside className="container"></aside>
+      <main>Hello world</main>
     </section>
   );
 }
