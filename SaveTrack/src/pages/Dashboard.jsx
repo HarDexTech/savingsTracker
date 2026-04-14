95a6bd6225996d39748dabc42cec03e1ece331be
KoryTECH <korylinemedia@gmail.com>
2026-04-14 17:04:41 +0100

    merge

--- SaveTrack/src/pages/Dashboard.jsx
+++ SaveTrack/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
+import Header from "../components/layout/Header.jsx"
 function DashBoard() {
   return (
-    <section className="bg-black h-dvh w-full">
+    <section className="bg-(--bg-app) h-dvh w-full flex flex-row items-start">
       <aside className="p-6 sidebar h-dvh w-75 bg-(--bg-surface-elevated)">
         <h1 class="text-2xl font-black text-[#ffb59d] uppercase tracking-tighter">
           SaveTrack
@@ -14,22 +15,15 @@ function DashBoard() {
           <button className="text-(--text-main)">History</button>
         </div>
       </aside>
-      <main></main>
 
-      <header className="flex bg-(--bg-app) p-4 justify-evenly items-center gap-5">
-        <h2 className="text-(--color-primary)">Dashboard Overview</h2>
-        <span className="flex justify-center items-center gap-3">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Search Portfolio"
-            className="w-98 h-10 bg-(--bg-surface-elevated) rounded-full text-(--text-muted) px-4 outline-none"
-          />
-          <i class="fas fa-bell text-(--text-muted) text-2xl"></i>
-          <i class="fas fa-cog text-(--text-muted) text-2xl"></i>
-        </span>
-      </header>
+      <Header></Header>
+      <main>
+        <article className="">
+          <div className=""></div>
+          <div className=""></div>
+          <div className=""></div>
+        </article>
+      </main>
     </section>
   );
 }
