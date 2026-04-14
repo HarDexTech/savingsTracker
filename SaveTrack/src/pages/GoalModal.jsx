167767342af783ede01dc22d720d9b271d3a6f47
KoryTECH <korylinemedia@gmail.com>
2026-04-14 15:02:13 +0100

    Refactor GoalModal component layout and styling for improved structure and responsiveness

--- SaveTrack/src/pages/GoalModal.jsx
+++ SaveTrack/src/pages/GoalModal.jsx
@@ -1,7 +1,15 @@
 function GoalModal(){
     return(
-        <section class="bg-black w-full h-[100vh]">
-            <div></div>
+        <section className="bg-black w-full h-[100vh] flex justify-center items-center">
+            <div className="w-60 h-72 bg-white rounded-xl p-4">
+                <div className="">
+                    <span className="flex flex-col">
+                        <p>Add new Goal</p>
+                        <p className="">Define your next financial milestone</p>
+                    </span>
+                    <span className=""></span>
+                </div>
+            </div>
         </section>
     )
 }
