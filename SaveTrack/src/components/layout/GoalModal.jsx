3b4f7093b98b33f4b782e4cc0251332df1a4ce9f
KoryTECH <korylinemedia@gmail.com>
2026-04-15 12:02:16 +0100

    Add onClick handler to Button component and create GoalModal component

---
+++ SaveTrack/src/components/layout/GoalModal.jsx
@@ -0,0 +1,13 @@
+import Dashboard from "../src/pages/Dashboard";
+function GoalModal(){
+    return(
+        <><Dashboard className="z-0" />
+        <section className="backdrop-blur-sm fixed top-0 left-0 w-full h-full flex items-center justify-center z-10 bg-(--bg-surface)">
+            
+        </section></>
+
+
+    )
+}
+
+export default GoalModal;
--- SaveTrack/src/components/ui/Button.jsx
+++ SaveTrack/src/components/ui/Button.jsx
@@ -1,10 +1,14 @@
-function Button({destination, active}) {
+function Button({destination, active, onClick}) {
   return active ? (
-    <button className="text-(--text-main) bg-linear-to-r from-[#ffb59d] to-[#f06536] rounded-lg shadow-[0_0_20px_rgba(240,101,54,0.3)] flex items-center space-x-3 p-3 font-['Arial'] font-medium text-sm tracking-wide transition-transform duration-200 scale-95 active:scale-90 cursor-pointer w-full">
+    <button 
+      onClick={onClick}
+      className="text-(--text-main) bg-linear-to-r from-[#ffb59d] to-[#f06536] rounded-lg shadow-[0_0_20px_rgba(240,101,54,0.3)] flex items-center space-x-3 p-3 font-['Arial'] font-medium text-sm tracking-wide transition-transform duration-200 scale-95 active:scale-90 cursor-pointer w-full">
       {destination}
     </button>
   ) : (
-    <button className="text-(--text-main) rounded-lg flex items-center space-x-3 p-3 font-['Arial'] font-medium text-sm tracking-wide transition-transform duration-200 scale-95 active:scale-90 cursor-pointer w-full hover:shadow-[0_0_25px_rgba(240,101,54,0.5)]">
+    <button 
+      onClick={onClick}
+      className="text-(--text-main) rounded-lg flex items-center space-x-3 p-3 font-['Arial'] font-medium text-sm tracking-wide transition-transform duration-200 scale-95 active:scale-90 cursor-pointer w-full hover:shadow-[0_0_25px_rgba(240,101,54,0.5)]">
       {destination}
     </button>
   );
