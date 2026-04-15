6f7fdf44a351e590fa77e0515310c1bf27785817
KoryTECH <korylinemedia@gmail.com>
2026-04-15 13:02:25 +0100

    Add GoalModal component with form for creating new savings goals

--- SaveTrack/src/main.jsx
+++ SaveTrack/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import DashBoard from './pages/Dashboard.jsx';
+import DashBoard from './components/layout/GoalModal.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DashBoard />
+    <GoalModal />
   </StrictMode>,
 );
